3499dfb23f057cf4d5310db008f206c9bc1ca242
playLoL <playza.1987@gmail.com>
2026-03-10 22:46:28 +0700

    lab24_3.cpp completed

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
-//Write function count() here.
+int count(int arr[], int size){
+    int unique = 0;
+    for(int i = 0; i < size; i++){
+        bool found = false;
+        for(int j = 0; j < i; j++){
+            if(arr[j] == arr[i]){
+                found = true;
+                break;
+            }
+        }
+        if(!found) unique++;
+    }
+    return unique;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
